e17fbd79aa7a862e5cdfa7c145f58d6f6848d77a
Kevin Amoako-Farkye <kevinfarkye130@gmail.com>
2026-09-11 20:44:57 -0400

    Lesson 0 Prework

---
+++ launch_console.py
@@ -0,0 +1,24 @@
+print("Hello there, fellow programmer!")
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}. Welcome to the Launch Console!")
+ 
+print("Pick one of the following options to learn about me: ")
+print("1. About Me")
+print("2. My Goals")
+print("3. Education")
+print("4. Exit")
+choice = input('')
+
+program = True
+while program:
+  if choice == "1":
+    print("My name is Kevin. I am a 15 (almost 16) year old senior from Greensboro, NC.")
+  elif choice == "2":
+    print("I want to become a mechanical engineer. I am learning Pyhton to help me build knowledge and gain experience.")
+  elif choice == "3":
+    print("I have earned a certificate in AutoDesk AutoCAD from my Drafting I class, and I am going to earn an AutoDesk Inventor certificate from my Drafting II class at the end of the school year.")
+  elif choice == "4":
+    print("Goodbye!")
+    program = False
+  else:
+    print("Please choose a number 1-4.")
